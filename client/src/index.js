e7fc9c41b7e1f0d956fd7cc16152b40cc3c6fb6a
cassio.espindola <cassio.espindola@conductor.com.br>
2019-06-18 12:04:02 -0300

    Listing streams

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,11 @@ import ReactDOM from 'react-dom';
 import reducers from './reducers';
 import App from './components/App';
 import {Provider} from "react-redux";
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import reduxThunk from 'redux-thunk';
 
-const store = createStore(reducers, applyMiddleware(reduxThunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
 
 ReactDOM.render(
     <Provider store={store}>
